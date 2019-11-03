eab7dc1751e678b3ab18b6395b626c6be58631f6
Fenton Taylor <fentontaylor@gmail.com>
2019-11-03 16:30:28 -0700

    Add expectations for current weather and summary

--- spec/requests/api/v1/forecast_spec.rb
+++ spec/requests/api/v1/forecast_spec.rb
@@ -15,6 +15,19 @@ describe '/api/v1/forecast endpoint' do
     expect(data[:data][:location][:latitude]).to eq(39.7392358)
     expect(data[:data][:location][:longitude]).to eq(-104.990251)
 
-    
+    expect(data[:data][:current][:time]).to eq('4:26 PM')
+    expect(data[:data][:current][:date]).to eq('11/02')
+    expect(data[:data][:current][:summary]).to eq('Partly Cloudy')
+    expect(data[:data][:current][:icon]).to eq('partly-cloudy-day')
+    expect(data[:data][:current][:temperature]).to eq(44.26)
+    expect(data[:data][:current][:feels_like]).to eq(40.75)
+    expect(data[:data][:current][:humidity]).to eq(0.42)
+    expect(data[:data][:current][:visibility]).to eq(10)
+    expect(data[:data][:current][:uv_index]).to eq(1)
+
+    expect(data[:data][:summary][:today]).to eq('Partly cloudy throughout the day.')
+    expect(data[:data][:summary][:tonight]).to eq('Clear')
+    expect(data[:data][:summary][:temp_high]).to eq(51.75)
+    expect(data[:data][:summary][:temp_low]).to eq(27.56)
   end
 end
