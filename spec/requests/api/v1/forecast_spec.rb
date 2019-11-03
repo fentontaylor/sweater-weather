62663a960dc243abb7a4a1169fed3ae3e81befe4
Fenton Taylor <fentontaylor@gmail.com>
2019-11-03 15:54:57 -0700

    Add expectations for location data

--- spec/requests/api/v1/forecast_spec.rb
+++ spec/requests/api/v1/forecast_spec.rb
@@ -1,11 +1,20 @@
-require 'rails_helper'
+require 'mock_helper'
 
 describe '/api/v1/forecast endpoint' do
   it 'returns forecast data for a specific city' do
+    stub_denver_location
+
     get '/api/v1/forecast?location=denver,co'
     expect(response).to be_successful
 
     data = JSON.parse(response.body, symbolize_names: true)
+
+    expect(data[:data][:location][:city]).to eq('Denver')
+    expect(data[:data][:location][:state]).to eq('CO')
+    expect(data[:data][:location][:country]).to eq('United States')
+    expect(data[:data][:location][:latitude]).to eq(39.7392358)
+    expect(data[:data][:location][:longitude]).to eq(-104.990251)
+
     
   end
 end
