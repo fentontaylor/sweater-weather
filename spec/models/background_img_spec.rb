639b445b5691aff1f188df79f6e27b1c7ca14361
Fenton Taylor <fentontaylor@gmail.com>
2019-11-04 16:07:21 -0700

    Add test and model for BackgroundImg

--- spec/models/background_img_spec.rb
+++ spec/models/background_img_spec.rb
@@ -11,4 +11,14 @@ describe BackgroundImg do
   it 'can initialize with a hash' do
     expect(@bg_img).to be_an_instance_of(BackgroundImg)
   end
+
+  it 'attributes' do
+    expect(@bg_img.results).to be_a Array
+  end
+
+  describe 'instance methods' do
+    it '#random_url' do
+      expect(@bg_img.random_url).to include('https://images.unsplash.com/photo')
+    end
+  end
 end
