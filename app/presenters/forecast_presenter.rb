5956626437d3f05a7736967acb9aa7a5042344c1
Fenton Taylor <fentontaylor@gmail.com>
2019-11-03 17:53:35 -0700

    Add new methods to presenter

--- app/presenters/forecast_presenter.rb
+++ app/presenters/forecast_presenter.rb
@@ -23,13 +23,77 @@ class ForecastPresenter
     geolocation.longitude
   end
 
-  private
-
-  def geolocation
-    @geolocation ||= Geolocation.new(geocode_service.get_location_data)
+  def time
+    forecast_decorator.time
   end
+
+  def date
+    forecast_decorator.date
+  end
+
+  def summary_current
+    forecast_decorator.summary_current
+  end
+
+  def summary_today
+    forecast_decorator.summary_today
+  end
+
+  def summary_tonight
+    forecast_decorator.summary_tonight
+  end
+
+  def icon
+    forecast_decorator.icon
+  end
+
+  def current_temp
+    forecast_decorator.current_temp
+  end
+
+  def feels_like
+    forecast_decorator.feels_like
+  end
+
+  def humidity
+    forecast_decorator.humidity
+  end
+
+  def visibility
+    forecast_decorator.visibility
+  end
+
+  def uv_index
+    forecast_decorator.uv_index
+  end
+
+  def temp_high
+    forecast_decorator.temp_high
+  end
+
+  def temp_low
+    forecast_decorator.temp_low
+  end
+
+  private
 
   def geocode_service
     @geocode_service ||= GeocodeService.new(@location)
+  end
+
+  def forecast_service
+    @forecast_service ||= ForecastService.new(geolocation.lat_long_string)
+  end
+
+  def geolocation
+    @geolocation ||= Geolocation.new(geocode_service.get_location)
+  end
+
+  def forecast
+    @forecast ||= Forecast.new(forecast_service.get_forecast)
+  end
+
+  def forecast_decorator
+    @forecast_decorator ||= ForecastDecorator.new(forecast)
   end
 end
