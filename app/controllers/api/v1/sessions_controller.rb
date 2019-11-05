b4c3c4f37985b30b2e8d33737ef2a3a4304914d5
Fenton Taylor <fentontaylor@gmail.com>
2019-11-05 09:11:57 -0700

    Refactor conntroller to abstract json responses

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -4,7 +4,19 @@ class Api::V1::SessionsController < ApplicationController
     user = User.find_by(email: session_params[:email])
     if user&.authenticate(session_params[:password])
       session[:user_id] = user.id
-      render json: { api_key: user.api_key }, status: 200
+      render_success
+    else
+      render_unauthorized
     end
+  end
+
+  private
+
+  def render_success
+    render json: { api_key: user.api_key }, status: 200
+  end
+
+  def render_unauthorized
+    render json: { errors: 'Invalid email or password' }, status: 401
   end
 end
--- spec/requests/api/v1/post_session_spec.rb
+++ spec/requests/api/v1/post_session_spec.rb
@@ -20,10 +20,26 @@ describe 'POST request with credentials sent to /api/v1/sessions', type: :reques
       post '/api/v1/sessions', params: @credentials, headers: @headers
 
       body = JSON.parse(response.body, symbolize_names: true)
-      
+
       expect(response.status).to eq(200)
       expect(body[:api_key]).to be_a String
       expect(body[:api_key].length).to eq(48)
+    end
+  end
+
+  context 'POST body contains incorrect credentials' do
+    it 'returns error message, status 401'  do
+      bad_credentials = '{
+        "email": "bob@ross.com",
+        "password": "tree_is_bad"
+      }'
+
+      post '/api/v1/sessions', params: bad_credentials, headers: @headers
+
+      body = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response.status).to eq(401)
+      expect(body[:errors]).to eq('Invalid email or password')
     end
   end
 end
