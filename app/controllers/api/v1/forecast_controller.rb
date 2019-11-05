c98c85ef02d768f3569b0da744e9aa4024741486
Fenton Taylor <fentontaylor@gmail.com>
2019-11-05 13:47:51 -0700

    Edit forecast controller to match new forecast facade name

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::ForecastController < ApplicationController
   def index
-    presenter = ForecastPresenter.new(params[:location])
-    render json: ForecastSerializer.new(presenter).json
+    facade = ForecastFacade.new(params[:location])
+    render json: ForecastSerializer.new(facade)
   end
 end
