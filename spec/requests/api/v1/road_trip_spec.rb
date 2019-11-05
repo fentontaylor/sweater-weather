6a6ce00901c7d8e0f568d4184ff5eaa71072092c
Fenton Taylor <fentontaylor@gmail.com>
2019-11-05 11:28:15 -0700

    Add routes

--- spec/requests/api/v1/road_trip_spec.rb
+++ spec/requests/api/v1/road_trip_spec.rb
@@ -9,9 +9,20 @@ describe 'POST /api/v1/road_trip', type: :request do
       "destination" => "Pueblo,CO",
       "api_key" => "#{@user.api_key}"
     }.to_json
+
+    @headers = {
+      'Content-Type' => 'application/json',
+      'Accept' => 'application/json'
+    }
   end
 
   it 'returns a forecast for estimated arrival time in destination city' do
+    post '/api/v1/road_trip', params: @body, headers: @headers
 
+    expect(response.status).to eq(200)
+
+    data = JSON.parse(response.body, symbolize_names: true)
+
+    expect(data).to be_a Hash
   end
 end
