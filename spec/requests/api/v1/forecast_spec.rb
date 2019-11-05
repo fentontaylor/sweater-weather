7082fd6e9b589a400584fa7b4d46540a09ae2afa
Fenton Taylor <fentontaylor@gmail.com>
2019-11-05 13:47:13 -0700

    Refactor test to match simplified JSON format

--- spec/requests/api/v1/forecast_spec.rb
+++ spec/requests/api/v1/forecast_spec.rb
@@ -16,29 +16,30 @@ describe '/api/v1/forecast endpoint', type: :request do
     expect(data[:data][:location][:latitude]).to eq(39.7392358)
     expect(data[:data][:location][:longitude]).to eq(-104.990251)
 
-    forecast = data[:data][:forecast]
-    expect(forecast[:current][:time]).to eq('4:26 PM')
-    expect(forecast[:current][:date]).to eq('11/02')
-    expect(forecast[:current][:summary]).to eq('Partly Cloudy')
-    expect(forecast[:current][:icon]).to eq('partly-cloudy-day')
-    expect(forecast[:current][:temperature]).to eq(44.26)
-    expect(forecast[:current][:feels_like]).to eq(40.75)
-    expect(forecast[:current][:humidity]).to eq(0.42)
-    expect(forecast[:current][:visibility]).to eq(10)
-    expect(forecast[:current][:uv_index]).to eq(1)
-
+    summary = data[:data][:attributes][:summary]
     expect(forecast[:summary][:today]).to eq('Partly cloudy throughout the day.')
     expect(forecast[:summary][:tonight]).to eq('Clear')
     expect(forecast[:summary][:temp_high]).to eq(51.75)
     expect(forecast[:summary][:temp_low]).to eq(27.56)
 
-    hourly = forecast[:hourly][:data]
+    current = data[:data][:attributes][:current]
+    expect(current[:time]).to eq('4:26 PM')
+    expect(current[:date]).to eq('11/02')
+    expect(current[:summary]).to eq('Partly Cloudy')
+    expect(current[:icon]).to eq('partly-cloudy-day')
+    expect(current[:temperature]).to eq(44.26)
+    expect(current[:feels_like]).to eq(40.75)
+    expect(current[:humidity]).to eq(0.42)
+    expect(current[:visibility]).to eq(10)
+    expect(current[:uv_index]).to eq(1)
+
+    hourly = data[:data][:attributes][:hourly]
     expect(hourly.count).to eq(8)
     expect(hourly.first[:time]).to eq('4 PM')
     expect(hourly.first[:icon]).to eq("partly-cloudy-day")
     expect(hourly.first[:temperature]).to eq(45.41)
 
-    daily = forecast[:daily][:data]
+    daily = data[:data][:attributes][:daily]
     expect(daily.count).to eq(5)
     expect(daily.first[:day]).to eq('Saturday')
     expect(daily.first[:icon]).to eq('clear-day')
