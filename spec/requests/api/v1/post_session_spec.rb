f35e82f8fe4a4330d8eb9598470fae4e911ed72c
Fenton Taylor <fentontaylor@gmail.com>
2019-11-05 08:38:56 -0700

    Add test for session creation

---
+++ spec/requests/api/v1/post_session_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+describe 'POST request with credentials sent to /api/v1/sessions', type: :request do
+  before :each do
+    @user = User.create(email: 'bob@ross.com', password: 'tree', password_confirmation: 'tree' )
+
+    @credentials = '{
+      "email": "bob@ross.com",
+      "password": "tree"
+    }'
+
+    @headers = {
+      'Content-Type' => 'application/json',
+      'Accept' => 'application/json'
+    }
+  end
+
+  context 'POST body contains correct credentials' do
+    it 'creates a session and returns their api key'  do
+      post '/api/v1/sessions', params: @credentials, headers: @headers
+
+      expect(response.status).to eq(200)
+      expect(body[:api_key]).to be_a String
+      expect(body[:api_key].length).to eq(48)
+    end
+  end
+end
