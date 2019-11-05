8e107fbfdcf8dd6676b6c51c63f9729730b51e53
Fenton Taylor <fentontaylor@gmail.com>
2019-11-05 14:58:22 -0700

    Update forecast facade and serializer for new object-oriented serialization

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -7,80 +7,16 @@ class ForecastFacade
     @type = 'forecast'
   end
 
-  def city
-    geolocation.city
-  end
-
-  def state
-    geolocation.state
-  end
-
-  def country
-    geolocation.country
-  end
-
-  def latitude
-    geolocation.latitude
-  end
-
-  def longitude
-    geolocation.longitude
-  end
-
-  def time
-    forecast_decorator.time
-  end
-
-  def date
-    forecast_decorator.date
-  end
-
-  def summary_current
-    forecast_decorator.summary_current
-  end
-
-  def summary_today
-    forecast_decorator.summary_today
-  end
-
-  def summary_tonight
-    forecast_decorator.summary_tonight
-  end
-
-  def icon
-    forecast_decorator.icon
-  end
-
-  def temp_current
-    forecast_decorator.temp_current
-  end
-
-  def feels_like
-    forecast_decorator.feels_like
-  end
-
-  def humidity
-    forecast_decorator.humidity
-  end
-
-  def visibility
-    forecast_decorator.visibility
-  end
-
-  def uv_index
-    forecast_decorator.uv_index
-  end
-
-  def temp_high
-    forecast_decorator.temp_high
-  end
-
-  def temp_low
-    forecast_decorator.temp_low
+  def location_info
+    geolocation_decorator.location_info
   end
 
   def summary
     forecast_decorator.summary
+  end
+
+  def current_forecast
+    forecast_decorator.current_forecast
   end
 
   def hourly_forecast
@@ -103,6 +39,10 @@ class ForecastFacade
 
   def geolocation
     @geolocation ||= Geolocation.new(geocode_service.get_location)
+  end
+
+  def geolocation_decorator
+    @geolocation_decorator ||= GeolocationDecorator.new(geolocation)
   end
 
   def forecast
--- app/serializers/forecast_serializer.rb
+++ app/serializers/forecast_serializer.rb
@@ -1,8 +1,16 @@
 class ForecastSerializer
   include FastJsonapi::ObjectSerializer
 
+  attribute :location do |obj|
+    obj.location_info
+  end
+
   attribute :summary do |obj|
     obj.summary
+  end
+
+  attribute :current do |obj|
+    obj.current_forecast
   end
 
   attribute :hourly do |obj|
