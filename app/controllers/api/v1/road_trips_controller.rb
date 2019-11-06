171344866426448d43ec27071b0eddab406b1f88
Fenton Taylor <fentontaylor@gmail.com>
2019-11-05 19:04:49 -0700

    Add tests and controller logic for rendering 401 response

--- app/controllers/api/v1/road_trips_controller.rb
+++ app/controllers/api/v1/road_trips_controller.rb
@@ -4,6 +4,14 @@ class Api::V1::RoadTripsController < ApplicationController
     if User.find_by(api_key: body[:api_key])
       facade = RoadTripFacade.new(body)
       render json: RoadTripSerializer.new(facade)
+    else
+      render_error_response
     end
+  end
+
+  private
+
+  def render_error_response
+    render json: { status: 401, errors: 'Invalid or missing key' }, status: 401
   end
 end
--- spec/requests/api/v1/road_trip_spec.rb
+++ spec/requests/api/v1/road_trip_spec.rb
@@ -3,6 +3,10 @@ require 'mock_helper'
 describe 'POST /api/v1/road_trip', type: :request do
   WebMock.allow_net_connect!
   before :each do
+    stub_denver_to_pueblo_directions
+    stub_pueblo_future_forecast
+
+    # Timecop.freeze(Time.at 1573009319)
     @user = User.create(email: 'bob@ross.com', password: 'tree', password_confirmation: 'tree' )
 
     @body = {
@@ -18,12 +22,14 @@ describe 'POST /api/v1/road_trip', type: :request do
   end
 
   it 'returns a forecast for estimated arrival time in destination city' do
+    # frozen_time = Time.at 1573009319
+    # Time.stubs(:now).returns(frozen_time)
     post '/api/v1/road_trip', params: @body, headers: @headers
 
     expect(response.status).to eq(200)
 
     data = JSON.parse(response.body, symbolize_names: true)
-    binding.pry
+
     expect(data).to have_key(:data)
     expect(data[:data][:id]).to be_nil
     expect(data[:data][:type]).to eq('road_trip')
@@ -32,5 +38,38 @@ describe 'POST /api/v1/road_trip', type: :request do
     expect(attributes[:temperature]).to eq(40)
     expect(attributes[:summary]).to eq('Clearly')
     expect(attributes[:approx_travel_time]).to eq('5555')
+  end
+
+  it 'returns status 401 with error message if key invalid' do
+    body = {
+      "origin" => "Denver,CO",
+      "destination" => "Pueblo,CO",
+      "api_key" => "#{@user.api_key}+invalidation"
+    }.to_json
+
+    post '/api/v1/road_trip', params: body, headers: @headers
+
+    expect(response.status).to eq(401)
+
+    data = JSON.parse(response.body, symbolize_names: true)
+
+    expect(data[:status]).to eq(401)
+    expect(data[:errors]).to eq('Invalid or missing key')
+  end
+
+  it 'returns status 401 with error message if key missing' do
+    body = {
+      "origin" => "Denver,CO",
+      "destination" => "Pueblo,CO"
+    }.to_json
+
+    post '/api/v1/road_trip', params: body, headers: @headers
+
+    expect(response.status).to eq(401)
+
+    data = JSON.parse(response.body, symbolize_names: true)
+
+    expect(data[:status]).to eq(401)
+    expect(data[:errors]).to eq('Invalid or missing key')
   end
 end
