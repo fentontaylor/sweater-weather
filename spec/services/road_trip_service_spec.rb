9b1649c9598d94979cf2ef068116876fc9a6273a
Fenton Taylor <fentontaylor@gmail.com>
2019-11-05 18:35:21 -0700

    Stub api call in RoadTripService test

--- spec/services/road_trip_service_spec.rb
+++ spec/services/road_trip_service_spec.rb
@@ -1,8 +1,9 @@
 require 'mock_helper'
 
 describe RoadTripService do
-  WebMock.allow_net_connect!
   it 'takes origin and destination and returns data from Google Directions API' do
+    stub_denver_to_pueblo_directions
+
     service = RoadTripService.new('Denver,CO', 'Pueblo,CO')
     data = service.get_directions
 
